d570902d6dda05bfd313cc90fb852347f63c5857
任晓航 <renxiaohang@innobuddy.com>
2023-05-10 18:51:31 +0800

    feat: 补充

---
+++ src/files/index.js
@@ -0,0 +1,46 @@
+import { OpenAI } from "langchain/llms/openai";
+import { RetrievalQAChain } from "langchain/chains";
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { CharacterTextSplitter } from "langchain/text_splitter";
+import * as fs from "fs";
+
+export const run = async () => {
+    const model = new OpenAI({ openAIApiKey: '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' });
+    const text = fs.readFileSync("诗词曲.txt", "utf8");
+    const textSplitter = new CharacterTextSplitter({
+        separator: '  ',
+        chunkSize: 10,
+        chunkOverlap: 2,
+    });
+    const docs = await textSplitter.createDocuments([text]);
+
+
+    console.log(docs)
+
+    const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings({ openAIApiKey: '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' }));
+
+
+    const result = await vectorStore.similaritySearchWithScore("你能介绍一下群鹤咏吗", 1);
+    // console.log(result)
+
+
+    // const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
+    // const res = await chain.call({
+    //     query: "你好，你喜欢群鹤咏这首诗吗？你能介绍一下吗？",
+    // }
+    // )
+    // console.log({ res },
+    //     // chain,
+    //     // chain.retriever.vectorStore.args,
+    //     // chain.combineDocumentsChain.llmChain.prompt.inputVariables,
+    //     // chain.combineDocumentsChain.llmChain.llm.caller,
+    //     // '-----------------------------------',
+    //     // chain.combineDocumentsChain.llmChain.llm.clinent,
+    //     // chain.combineDocumentsChain.llmChain.llm.clientConfig,
+    // );
+}
+
+
+run()
